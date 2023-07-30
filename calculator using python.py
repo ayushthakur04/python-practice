068326047b33fd55c7a094671f88361b445e6e9d
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-07-31 02:53:44 +0530

    Create calculator using python.py

---
+++ calculator using python.py
@@ -0,0 +1,23 @@
+a=int(input("Enter first number :"))
+b=input("Enter the operator(+,-,*,/) : ")
+c=int(input("Enter second number : "))
+if b =="+" :
+    print(a+c)
+elif b == "-":
+    print(a-c)
+elif b == "*":
+    print(a*c)
+elif b == "/":
+    print(a/c)
+else :
+    print("invalid expression :(")
+
+
+
+
+
+
+Enter first number :4
+Enter the operator(+,-,*,/) : +
+Enter second number : 4
+8
